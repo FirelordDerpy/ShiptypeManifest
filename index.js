d12b110aab9479f47685c5496e6efcfbae99281c
FirelordDerpy <Daniellanier@hotmail.com>
2024-07-10 17:33:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -114,16 +114,56 @@ class Ship {
 
 // name { price, power level,}
 const addons = {
-    'Shield': { cost: 10, powerLevelBoost: 10 },
-    'Laser': { cost: 20, powerLevelBoost: 20 },
-    'Rocket': { cost: 30, powerLevelBoost: 30 }
+    'None': { cost: 0, powerLevelBoost: 0 },
+    'Advanced Shields': { cost: 1, powerLevelBoost: 1 },
+    'Heavy Armor': { cost: 1, powerLevelBoost: 1 },
+    'High Speed': { cost: 1.5, powerLevelBoost: 1.5 },
+    'Tractor Beam': { cost: 1, powerLevelBoost: 1 },
+    'Ion Bombs': { cost: 1.3, powerLevelBoost: 1.3 },
+    'Ion Torpedoes': { cost: 1.4, powerLevelBoost: 1.4 },
+    'Massive Hangers 3+': { cost: 1, powerLevelBoost: 1 },
+    'Interdiction Generator 6+': { cost: 2.5, powerLevelBoost: 2.5 },
+    'Recon array': { cost: 1, powerLevelBoost: 1 },
+    'Transport': { cost: 1, powerLevelBoost: 1 },
+    'Advanced Computers': { cost: 1.1, powerLevelBoost: 1.1 },
+    'Hyperdrive': { cost: 1, powerLevelBoost: 1 },
+    'Flagship': { cost: 5, powerLevelBoost: 5 },
+    'BOMB SHIP': { cost: 2, powerLevelBoost: 2 },
+    'Comms Jammer': { cost: 2.5, powerLevelBoost: 2.5 },
+    'Grabbing Claws': { cost: 1, powerLevelBoost: 1 },
+    'Ramming Hull 4+': { cost: 1, powerLevelBoost: 1 },
+    'Stealth Field': { cost: 4, powerLevelBoost: 4 },
+    'Minelayers 4+': { cost: 1, powerLevelBoost: 1 },
+    'Long Range Torpedoes': { cost: 1.2, powerLevelBoost: 1.2 },
+    'Flack Cannons 4+': { cost: 1.5, powerLevelBoost: 1.5 },
+    'Large Caliber Cannons 4+': { cost: 1.5, powerLevelBoost: 1.5 },
+    'Ground Bombardment Cannons 3+': { cost: 1.2, powerLevelBoost: 1.2 },
+    'Unguided Bombs': { cost: 1, powerLevelBoost: 1 },
+    'Guided Bombs': { cost: 1.5, powerLevelBoost: 1.5 },
+    'Cluster Bombs': { cost: 1.2, powerLevelBoost: 1.2 },
+    'Amphibious': { cost: 1.1, powerLevelBoost: 1.1 },
+    'Concussion Missiles': { cost: 1.2, powerLevelBoost: 1.2 },
+    'Proton Torpedos': { cost: 1.2, powerLevelBoost: 1.2 },
+    'Ion Cannons': { cost: 1.3, powerLevelBoost: 1.3 },
+    'Beam Cannons': { cost: 1.2, powerLevelBoost: 1.2 },
+    'MINE, Explosive': { cost: 1, powerLevelBoost: 1 },
+    'MINE, EMP': { cost: 1.1, powerLevelBoost: 1.1 },
+    'MINE, Anti-Shields': { cost: 1, powerLevelBoost: 1 },
+    'Independent Weapons Generator': { cost: 1.2, powerLevelBoost: 1.2 },
+    'Skylance Launcher': { cost: 3, powerLevelBoost: 3 },
+    'Massive Scale': { cost: 2, powerLevelBoost: 2 },
+    'Electro Cannon': { cost: 2, powerLevelBoost: 2 },
+    'Ship Upgrade Part': { cost: 1, powerLevelBoost: 1 },
+    'Ship Part': { cost: 1, powerLevelBoost: 1 },
+    'Civilian Vessel': { cost: 0.5, powerLevelBoost: 0.5 }
 };
+
 
 
 // Create some individual ships
 const ships = [
-    new Ship('Fighters', 'Fighter 1', ['Shield', 'Laser']),
-    new Ship('Interceptors', 'Interceptor 1', ['Shield', 'Rocket']),
+    new Ship('Fighters', 'Fighter 1', ['Heavy Armor', 'Heavy Armor']),
+    new Ship('Interceptors', 'Interceptor 1', ['Heavy Armor', 'Heavy Armor']),
     // Add more individual ships as needed
 ];
 
