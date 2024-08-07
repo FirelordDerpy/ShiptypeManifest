e710e585245fcea8914c317cf6d16a922347838f
FirelordDerpy <Daniellanier@hotmail.com>
2024-08-07 16:39:16 -0400

    2

--- shipyard.js
+++ shipyard.js
@@ -1,12 +1,16 @@
 export const manufacturers = ['Incom', 'MVC', 'Free Dac'];
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    const manufacturers = ['Incom', 'MVC', 'Free Dac'];
-    const list = document.getElementById('manufacturers-list');
+    if (window.location.pathname === '/shipyards.html') {
+        const manufacturers = ['Incom', 'MVC', 'Free Dac'];
+        const list = document.getElementById('manufacturers-list');
 
-    manufacturers.forEach(manufacturer => {
-        let listItem = document.createElement('li');
-        listItem.textContent = manufacturer;
-        list.appendChild(listItem);
-    });
+        if (list) {
+            manufacturers.forEach(manufacturer => {
+                let listItem = document.createElement('li');
+                listItem.textContent = manufacturer;
+                list.appendChild(listItem);
+            });
+        }
+    }
 });
