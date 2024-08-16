1ffc7b278fd439d96e57a1331fb366a132e6c863
FirelordDerpy <Daniellanier@hotmail.com>
2024-08-15 20:10:06 -0400

    Update shipyard.js

--- shipyard.js
+++ shipyard.js
@@ -6,7 +6,7 @@ import { push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-databas
 const appSettings = { databaseURL: "https://shiptypemanifest009-default-rtdb.firebaseio.com/" };
 const app = initializeApp(appSettings);
 const db = getDatabase(app);
-const completeAudio = new Audio('/assets/complete.wav');
+const completeAudio = new Audio('assets\complete.wav');
 
 let buildTimeModifier = 1.5;
 let userCredits = 1000000000000; 
