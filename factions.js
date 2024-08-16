1b8fa503ed0caedaab10a7b42acbaa9ab4cee164
FirelordDerpy <Daniellanier@hotmail.com>
2024-08-16 19:02:19 -0400

    Update factions.js

--- factions.js
+++ factions.js
@@ -3,30 +3,23 @@ import { set, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-dat
 
 let clientInput, addClientButton, clientList;
 
-// Load the clients array from localStorage or initialize it as an empty array
 let clients = JSON.parse(localStorage.getItem('clients')) || [];
 
 function addClient() {
     const clientName = clientInput.value;
     if (clientName) {
-        // Add the client name to the list on the HTML page
         const listItem = document.createElement('li');
         listItem.textContent = clientName;
         clientList.appendChild(listItem);
-        // Add the client name to the clients array
         clients.push(clientName);
-        // Save the clients array to localStorage
         localStorage.setItem('clients', JSON.stringify(clients));
-        // Clear the input field
         clientInput.value = '';
     }
 }
 
 function displayClients() {
-    // Clear the old list
     clientList.innerHTML = '';
 
-    // Add each client name in the clients array to the list on the HTML page
     for (const clientName of clients) {
         const listItem = document.createElement('li');
         listItem.textContent = clientName;
@@ -40,7 +33,6 @@ function displayClients() {
 
         addClientButton.addEventListener('click', addClient);
 
-        // Display the clients when the page loads
         displayClients();
     }
 
